7bd397e3ddde1c8e24cff817d41611c2921a02be
przepiorar <przepiorar@wp.pl>
2018-12-10 21:26:36 +0100

    quake system poruszania

---
+++ Quake FPS/Assets/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+    
+    public float speedturn;
+    float min = 330f;
+    float max = 30f;
+
+    void FixedUpdate()
+    {
+        //transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0)  * speedturn);
+             transform.Rotate(new Vector3(-1 * Input.GetAxis("Mouse Y"), 0, 0) * speedturn);  //dobre
+
+        Debug.Log(transform.eulerAngles.x);
+        if (transform.eulerAngles.x < min && transform.eulerAngles.x >200f)  // 360 pelny obrot czyli tutaj 315
+            transform.rotation = Quaternion.Euler(min, transform.eulerAngles.y, transform.eulerAngles.z);
+        // ten sam warunek xddd
+        else if (transform.eulerAngles.x > max && transform.eulerAngles.x< 200f)
+            transform.rotation = Quaternion.Euler(max, transform.eulerAngles.y, transform.eulerAngles.z);
+
+        //  angle = Mathf.Clamp(Input.GetAxis("Mouse Y")*-10, -90, 90) *speedturn;
+        // transform.localRotation = Quaternion.AngleAxis(angle, Vector3.right);
+
+
+
+        //  if (transform.eulerAngles > new Vector3(90,0,0))
+        // {
+        //     transform.rotation = new Quaternion(90, 0, 0,0);
+        // }
+    }
+}
---
+++ Quake FPS/Assets/Mover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    public float speedturn;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+       //  Vector3 move2 = new Vector3(moveHorizontal, moveVertical,0);
+        Vector3 movement = transform.forward * moveVertical * speed;
+        Vector3 movement2 = transform.right * moveHorizontal * speed;
+
+        // Apply this movement to the rigidbody's position.
+        rb.MovePosition(rb.position + movement);
+        rb.MovePosition(rb.position + movement2);
+
+        //rb.transform.position += new Vector3(moveHorizontal*speed,0, moveVertical*speed);  dobre
+        //transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0)  * speedturn);
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0)  * speedturn);  //dobre
+    }
+}
